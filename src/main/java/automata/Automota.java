01f6b49a1099f3f2ff85a1eb7cfc2655cb62f2ef
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2021-01-18 17:37:10 +0000

    first commit

---
+++ src/main/java/automata/Automota.java
@@ -0,0 +1,6 @@
+package automata;
+
+public interface Automota {
+    boolean accept(String input);
+    void create(String regex);
+}
---
+++ src/main/java/automata/NFA.java
@@ -0,0 +1,6 @@
+package automata;
+
+public class NFA implements Automota{
+    public void create(String regex){}
+    public boolean accept(String input){return true;}
+}
