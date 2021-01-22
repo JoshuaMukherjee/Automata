c8e6fda4ea60760c2eed07532885b3a1693b485e
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2021-01-22 16:35:54 +0000

    generators

--- src/main/java/automata/App.java
+++ src/main/java/automata/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( new NFA().accept("input") );
+        System.out.println( new GlushkovGen("hi").create() );
     }
 }
--- src/main/java/automata/GlushkovGen.java
+++ src/main/java/automata/GlushkovGen.java
@@ -1,12 +1,38 @@
 package automata;
 
-public class GlushkovGen implements Generator{
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class GlushkovGen implements Generator {
+
+    private Set<String> lambda;
+    private Set<String> P;
+    private Set<String> D;
+    private Set<String> F;
+    private String regex;
+
+    public GlushkovGen(String regex){
+        this.regex = regex;
+        lambda = CreateLambda();
+    }
 
     @Override
     public Automota create() {
         return new NFA();
     }
 
+    public Set<String> CreateLambda(){
+        Set<String> lambda = new HashSet<>();
+        if(regex.isEmpty()){
+            lambda = Collections.emptySet();
+            lambda.add("");
+        }
     
+
+        return lambda;
+    }
+
+
     
 }
