595842cd7be8a3fa2a91266c8553d6c4666c0cbd
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2021-01-22 16:35:37 +0000

    Parser

---
+++ src/main/java/automata/Parser.java
@@ -0,0 +1,21 @@
+package automata;
+
+
+/**
+ * regex = char | regex"|"regex | regexregex | regex* 
+ * char = {a-z}|{A-Z}|null
+ */
+public class Parser{
+    /**
+     * Returns {@code int} based on highest level type
+     * 
+     * 0 if not valid syntax
+     * 
+     * @param regex
+     * @return type of regex
+     */
+    public static int getType(String regex){
+        return 0;
+        
+    }
+}
