a5af7c8ec482a41c99c8bac53eec1dd734c35f6d
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2021-01-26 18:46:05 +0000

    Tests

--- src/test/java/automata/LL1Test.java
+++ src/test/java/automata/LL1Test.java
@@ -3,6 +3,7 @@ package automata;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -19,5 +20,37 @@ public class LL1Test {
         assertEquals(correct, tokens);
 
     }
+
+    @Test
+    public void LexerLongInput(){
+        LL1_Parser parser = new LL1_Parser();
+        List<Integer> tokens = parser.Lexer("(a|b)*|''");
+        List<Integer> correct = new ArrayList<>();
+        correct.addAll(Arrays.asList(2, 0, 3, 0, 1, 4, 3, 6, 5));
+        assertEquals(correct, tokens);
+    }
+
+    @Test
+    public void LexerQuotes(){
+        LL1_Parser parser = new LL1_Parser();
+        List<Integer> tokens = parser.Lexer("''");
+        List<Integer> correct = new ArrayList<>();
+        correct.addAll(Arrays.asList(6, 5));
+        assertEquals(correct, tokens);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void Lexer3Quotes(){
+        LL1_Parser parser = new LL1_Parser();
+        parser.Lexer("'''");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void LexerUnknownSymbol(){
+        LL1_Parser parser = new LL1_Parser();
+        parser.Lexer("§");
+    }
+
+
     
 }
