14469ce3e8f66eaf284ed1b78741245938bb3b2c
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2021-01-31 12:43:29 +0000

    cmd line

--- src/main/java/automata/App.java
+++ src/main/java/automata/App.java
@@ -8,9 +8,10 @@ public class App{
     public static void main( String[] args )
     {
         RegexParser p = new LL1_Parser();
+        System.out.println(args[0]);
         try{
             // p.getType("(a)");
-            System.out.println(p.parse("a|b*"));
+            System.out.println(p.parse(args[0]));
         } catch (RuntimeException e){
             System.err.println("error: " + e.getClass());
         }
